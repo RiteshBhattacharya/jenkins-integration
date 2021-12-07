095b8d264558d251c70d050ea9931c1e6b537e5a
RiteshBhattacharya <94947103+RiteshBhattacharya@users.noreply.github.com>
2021-12-07 15:22:08 +0530

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Guys I am Ritesh");
+        System.out.println("It is my first jenkins Project using aws and docker");
     }
 }
